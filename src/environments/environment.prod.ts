bebf948bc0db56f76a036ae6eb6e64b9d6ee42ca
ssubhan21 <mohdabdulsubhan3@gmail.com>
2025-03-14 12:15:36 -0400

    id addded

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     API_URL: 'https://subhan.vercel.app/api',
-
+    googleAnalyticsId: 'G-WHVHWSN92Y'
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   API_URL: 'https://subhan.vercel.app/api',
-  googleAnalyticsId: ''
+  googleAnalyticsId: 'G-WHVHWSN92Y'
 };
